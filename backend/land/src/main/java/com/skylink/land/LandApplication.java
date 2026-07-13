f7650866859eefd947e47984a36a7089044862b4
vanishingblank <15491509+vanishblank@user.noreply.gitee.com>
2026-07-13 17:03:02 +0800

    fix:remove scanner

--- backend/land/src/main/java/com/skylink/land/LandApplication.java
+++ backend/land/src/main/java/com/skylink/land/LandApplication.java
@@ -1,15 +1,12 @@
 package com.skylink.land;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.skylink.land.mapper")
 public class LandApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LandApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(LandApplication.class, args);
+    }
 }
