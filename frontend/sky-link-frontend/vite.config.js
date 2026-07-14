9a788db568e91f810d26b4198225aa6e3e05ae86
AleFlyX <1449466458@qq.com>
2026-07-14 17:31:46 +0800

    env: vite应用基础路径配置

--- frontend/sky-link-frontend/vite.config.js
+++ frontend/sky-link-frontend/vite.config.js
@@ -1,6 +1,7 @@
 import { fileURLToPath, URL } from 'node:url'
+import { cwd } from 'node:process'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import AutoImport from 'unplugin-auto-import/vite'
@@ -8,32 +9,37 @@ import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-    AutoImport({
-      dts: false,
-      resolvers: [ElementPlusResolver()],
-    }),
-    Components({
-      dts: false,
-      resolvers: [ElementPlusResolver()],
-    }),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
-  },
-  server: {
-    port: 5573,
-    proxy: {
-      '/api/v1': {
-        target: 'http://localhost:8080',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api\/v1/, ''),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, cwd(), '')
+
+  return {
+    base: env.BASE_URL || '/',
+    plugins: [
+      vue(),
+      vueDevTools(),
+      AutoImport({
+        dts: false,
+        resolvers: [ElementPlusResolver()],
+      }),
+      Components({
+        dts: false,
+        resolvers: [ElementPlusResolver()],
+      }),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
       },
     },
-  },
+    server: {
+      port: 5573,
+      proxy: {
+        '/api/v1': {
+          target: 'http://localhost:8080',
+          changeOrigin: true,
+          // rewrite: (path) => path.replace(/^\/api\/v1/, ''),
+        },
+      },
+    },
+  }
 })
