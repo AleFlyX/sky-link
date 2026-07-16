b33c9385f5ad089e8594413aa70d8c672d1c72eb
AleFlyX <1449466458@qq.com>
2026-07-16 08:02:37 +0800

    fix: elmessageBox样式问题修复，去除在入口文件的单独引用

--- frontend/sky-link-frontend/src/main.js
+++ frontend/sky-link-frontend/src/main.js
@@ -4,7 +4,6 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import { permission } from './directives'
-import 'element-plus/theme-chalk/el-message-box.css'
 import './assets/main.css'
 
 const app = createApp(App)
